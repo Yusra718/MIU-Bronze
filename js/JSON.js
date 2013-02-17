f7b46021ea0724ddc8b19f7daf3092d8b8db184c
Yusra Ahmed <Yusra718@fullsail.edu>
2013-02-16 22:27:58 -0500

    Entered files for Bronze app

---
+++ js/JSON.js
@@ -0,0 +1,202 @@
+var json = {
+	"decor1": 	{
+		"dectype": 		["Type: ", "Tinsel"],
+		"colorType": 	["Color Style: ", "Metallic"],
+		"colorAmount": 	["Multiple/Separate: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Blue","Pink","Purple"]],
+		"occasion": 	["Occasion: ", "Eid"],
+        "notes": 		["Notes: ", "Happy Eid"],
+        "typePack": 	["Business/Personal: ", "Personal"],
+        "packs": 		["How many packs? : ", "8"]
+	},
+	"decor2": 	{
+		"dectype": 		["Type: ", "Banners"],
+		"colorType": 	["Color Style: ", "Regular"],
+		"colorAmount": 	["Multiple/Separate: ", "Separate Colors"],
+		"colors": 		["Color(s): ", ["Green","Orange","Gray"]],
+		"occasion": 	["Occasion: ", "Birthday"],
+        "notes": 		["Notes: ", "Happy Birthday"],
+        "typePack": 	["Business/Personal: ", "Business"],
+        "packs": 		["How many packs? : ", "55"]
+	},
+	"decor3": 	{
+		"dectype": 		["Type: ", "Lights"],
+		"colorType": 	["Color Style: ", "Metallic"],
+		"colorAmount": 	["Multiple/Separate: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["White","Red"]],
+		"occasion": 	["Occasion: ", "Christmas"],
+        "notes": 		["Notes: ", "Merry Christmas"],
+        "typePack": 	["Business/Personal: ", "Personal"],
+        "packs": 		["How many packs? : ", "2"]
+	},
+	"decor4": 	{
+		"dectype": 		["Type: ", "Ceiling"],
+		"colorType": 	["Color Style: ", "Regular"],
+		"colorAmount": 	["Multiple/Separate: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Black","Orange"]],
+		"occasion": 	["Occasion: ", "Halloween"],
+		"notes": 		["Notes: ", "Happy Halloween"],
+        "typePack": 	["Business/Personal: ", "Business"],
+		"packs": 		["How many packs? : ", "75"]
+	},
+	"decor5": 	{
+		"dectype": 		["Type: ", "Ceiling"],
+		"colorType": 	["Color Style: ", "Metallic"],
+		"colorAmount": 	["Multiple/Separate: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Orange","Yellow"]],
+		"occasion": 	["Occasion: ", "Hanukkah"],
+		"notes": 		["Notes: ", "Happy Hanukkah"],
+        "typePack": 	["Business/Personal: ", "Business"],
+		"packs": 		["How many packs? : ", "45"]
+	},
+	"decor6": 	{
+		"dectype": 		["Type: ", "Tinsel"],
+		"colorType": 	["Color Style: ", "Regular"],
+		"colorAmount": 	["Multiple/Separate: ", "Separate Colors"],
+		"colors": 		["Color(s): ", ["Red","Orange","Yellow","Red","Blue"]],
+		"occasion": 	["Occasion: ", "Thanksgiving"],
+		"notes": 		["Notes: ", "Happy Thanksgiving"],
+        "typePack": 	["Business/Personal: ", "Business"],
+		"packs": 		["How many packs? : ", "75"]
+	},
+	"decor7": 	{
+		"dectype": 		["Type: ", "Balloons"],
+		"colorType": 	["Color Style: ", "Metallic"],
+		"colorAmount": 	["Multiple/Separate: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Pink","Purple","Red","White"]],
+		"occasion": 	["Occasion: ", "Quincenera"],
+		"notes": 		["Notes: ", "Happy 15th Birthday"],
+        "typePack": 	["Business/Personal: ", "Personal"],
+		"packs": 		["How many packs? : ", "10"]
+	},
+	"decor8": 	{
+		"dectype": 		["Type: ", "Balloons"],
+		"colorType": 	["Color Style: ", "Regular"],
+		"colorAmount": 	["Multiple/Separate: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Blue","Green","Red","Orange"]],
+		"occasion": 	["Occasion: ", "Bar Mitzvah"],
+		"notes": 		["Notes: ", "Happy 13th Birthday"],
+        "typePack": 	["Business/Personal: ", "Personal"],
+		"packs": 		["How many packs? : ", "6"]
+	},
+	"decor9": 	{
+		"dectype": 		["Type: ", "Banners"],
+		"colorType": 	["Color Style: ", "Metallic"],
+		"colorAmount": 	["Multiple/Separate: ", "Separate Colors"],
+		"colors": 		["Color(s): ", ["Pink","Purple"]],
+		"occasion": 	["Occasion: ", "Sweet 16"],
+		"notes": 		["Notes: ", "Happy Sweet 16"],
+        "typePack": 	["Business/Personal: ", "Business"],
+		"packs": 		["How many packs? : ", "55"]
+	},
+	"decor10": 	{
+		"dectype": 		["Type: ", "Tinsel"],
+		"colorType": 	["Color Style: ", "Regular"],
+		"colorAmount": 	["Multiple/Separate: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Pink","Red","Blue"]],
+		"occasion": 	["Occasion: ", "Engagement"],
+		"notes": 		["Notes: ", "Congratulations"],
+        "typePack": 	["Business/Personal: ", "Business"],
+		"packs": 		["How many packs? : ", "45"]
+	},
+	"decor11": 	{
+		"dectype": 		["Type: ", "Lights"],
+		"colorType": 	["Color Style: ", "Metallic"],
+		"colorAmount": 	["Multiple/Separate: ", "Separate Colors"],
+		"colors": 		["Color(s): ", ["Pink","Blue","Green"]],
+		"occasion": 	["Occasion: ", "Wedding"],
+		"notes": 		["Notes: ", "Congratulations"],
+        "typePack": 	["Business/Personal: ", "Personal"],
+		"packs": 		["How many packs? : ", "10"]
+	},
+	"decor12": 	{
+		"dectype": 		["Type: ", "Ceiling"],
+		"colorType": 	["Color Style: ", "Regular"],
+		"colorAmount": 	["Multiple/Separate: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Pink","Purple"]],
+		"occasion": 	["Occasion: ", "Baby Arrival"],
+		"notes": 		["Notes: ", "It's a Girl"],
+        "typePack": 	["Business/Personal: ", "Business"],
+		"packs": 		["How many packs? : ", "50"]
+	},
+	"decor13": 	{
+		"dectype": 		["Type: ", "Ceiling"],
+		"colorType": 	["Color Style: ", "Metallic"],
+		"colorAmount": 	["Multiple/Separate: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Blue","Green"]],
+		"occasion": 	["Occasion: ", "Baby Arrival"],
+		"notes": 		["Notes: ", "It's a Boy"],
+        "typePack": 	["Business/Personal: ", "Personal"],
+		"packs": 		["How many packs? : ", "1"]
+	},
+	"decor14": 	{
+		"dectype": 		["Type: ", "Tinsel"],
+		"colorType": 	["Color Style: ", "Regular"],
+		"colorAmount": 	["Multiple/Separate: ", "Separate Colors"],
+		"colors": 		["Color(s): ", ["Green","White"]],
+		"occasion": 	["Occasion: ", "St. Patrick's Day"],
+		"notes": 		["Notes: ", "Happy St. Patrick's Day"],
+        "typePack": 	["Business/Personal: ", "Personal"],
+		"packs": 		["How many packs? : ", "10"]
+	},
+	"decor15": 	{
+		"dectype": 		["Type: ", "Banners"],
+		"colorType": 	["Color Style: ", "Metallic"],
+		"colorAmount": 	["Multiple/Separate: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["White","Red"]],
+		"occasion": 	["Occasion: ", "Anniversary"],
+		"notes": 		["Notes: ", "Happy Anniversary"],
+        "typePack": 	["Business/Personal: ", "Business"],
+		"packs": 		["How many packs? : ","65"]
+	},
+	"decor16": 	{
+		"dectype": 		["Type: ", "Banners"],
+		"colorType": 	["Color Style: ", "Regular"],
+		"colorAmount": 	["Multiple/Separate: ", "Separate Colors"],
+		"colors": 		["Color(s): ", ["Blue","White"]],
+		"occasion": 	["Occasion: ", "Graduation"],
+		"notes": 		["Notes: ", "Congrats Grads"],
+        "typePack": 	["Business/Personal: ", "Business"],
+		"packs": 		["How many packs? : ", "70"]
+	},
+	"decor17": 	{
+		"dectype": 		["Type: ", "Tinsel"],
+		"colorType": 	["Color Style: ", "Metallic"],
+		"colorAmount": 	["Multiple/Separate: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["Red","White","Blue"]],
+		"occasion": 	["Occasion: ", "Independance Day"],
+		"notes": 		["Notes: ", "Happy 4th of July"],
+        "typePack": 	["Business/Personal: ", "Business"],
+		"packs": 		["How many packs? : ", "100"]
+	},
+	"decor18": 	{
+		"dectype": 		["Type: ", "Balloons"],
+		"colorType": 	["Color Style: ", "Regular"],
+		"colorAmount": 	["Multiple/Separate: ", "Separate Colors"],
+		"colors": 		["Color(s): ", ["Blue","Green","White"]],
+		"occasion": 	["Occasion: ", "Job Promotion"],
+		"notes": 		["Notes: ", "Congratulations"],
+        "typePack": 	["Business/Personal: ", "Business"],
+		"packs": 		["How many packs? : ", "90"]
+	},
+	"decor19": 	{
+		"dectype": 		["Type: ", "Lights"],
+		"colorType": 	["Color Style: ", "Metallic"],
+		"colorAmount": 	["Multiple/Separate: ", "Multiple Colors"],
+		"colors": 		["Color(s): ", ["White","Green"]],
+		"occasion": 	["Occasion: ", "Baby Shower"],
+		"notes": 		["Notes: ", "Congratulations"],
+        "typePack": 	["Business/Personal: ", "Business"],
+		"packs": 		["How many packs? : ", "35"]
+	},
+	"decor20": 	{
+		"dectype": 		["Type: ", "Ceiling"],
+		"colorType": 	["Color Style: ", "Regular"],
+		"colorAmount": 	["Multiple/Separate: ", "Separate Colors"],
+		"colors": 		["Color(s): ", ["Red","Pink","Blue"]],
+		"occasion": 	["Occasion: ", "Welcome Home"],
+		"notes": 		["Notes: ", "Welcome Home, We Missed You"],
+        "typePack": 	["Business/Personal: ", "Personal"],
+		"packs": 		["How many packs? : ", "5"]
+	}
+}
